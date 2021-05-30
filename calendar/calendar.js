c86048b133b33d49254d3298cb3df7df763681f7
Kamille <tramia25@gmail.com>
2021-05-30 19:04:54 -0400

    basic calendar update

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,43 +1,14 @@
 var p = MindFusion.Scheduling;
+// create a new instance of the calendar
+calendar = new p.Calendar(document.getElementById("calendar"));
 
-//create a new calendar instance
-var calendar = new p.Calendar(document.getElementById("calendar"));
-
-
-calendar.selectionEnd.addEventListener(handleSelection);
-calendar.headerClick.addEventListener(handleHeaderClick);
-
-//visualize the calendar
 calendar.render();
-function handleHeaderClick(sender, args)
-{
-	if(sender.currentView === p.CalendarView.Timetable)
-	{
-		sender.date = sender.timetableSettings.dates.items()[0];
-		sender.currentView = p.CalendarView.SingleMonth;
-	}
-}
 
-function handleSelection(sender, args)
-{
-	if(sender.currentView === p.CalendarView.SingleMonth)
-	{
-		//cancel the default behavior
-		args.cancel = true;
-		
-		var start = args.startTime;
-		var end = args.endTime;
-		
-		//clear all dates from the timetable
-		sender.timetableSettings.dates.clear();
-		
-		while(start < end)
-		{
-			sender.timetableSettings.dates.add(start);
-			start = p.DateTime.addDays(start, 1);
-			
-		}
-		
-		sender.currentView = p.CalendarView.Timetable;
-	}
-}
+// set the view to WeekRange
+calendar.currentView = CalendarView.WeekRange;
+
+// renders a single week
+calendar.weekRangeSettings.weekStyle = p.WeekRangeViewStyle.SingleWeek;
+calendar.weekRangeSettings.headerStyle = p.HorizontalHeaderStyle.None;
+calendar.weekRangeSettings.visibleRows = 5;
+ 
---
+++ controllers/homeRoutes.js
@@ -0,0 +1,106 @@
+const router = require('express').Router();
+const { Work, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Homepage
+router.get('/', async (req, res) => {
+    try {
+
+        const workData = await Work.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['first_name'],
+                },
+            ],
+        });
+        // Serialize data so the template can read it
+        const workers = workData.map((work) => work.get({ plain: true }));
+        // Pass serialized data and session flag into template
+        res.render('home', {
+            workers,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Find individual employees schedule
+router.get('/user/:id', async (req, res) => {
+    try {
+        const workData = await Work.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['first_name'],
+                },
+            ],
+        });
+
+        const work = workData.get({ plain: true });
+
+        res.render('calender', {
+            ...work,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Work }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('dashboard', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  // Use withAuth middleware to prevent access to route
+router.get('/schedule', withAuth, async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Work }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('scheduler', {
+        ...user,
+        logged_in: true,
+        is_manager: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+// If the user is already logged in, redirect the request to another route
+router.get('/home', (req, res) => {
+
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('home');
+});
+
+module.exports = router;
+
