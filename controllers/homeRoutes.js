dacd3d09db394682d30ba51c1ffef221cbd5b9bb
Vicheka Phat <vickphat@gmail.com>
2021-06-01 21:18:12 -0400

    checked routes in homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,6 +3,7 @@ const { Work, User } = require('../models');
 const withAuth = require('../utils/auth');
 
 // Homepage
+// WORKS
 router.get('/', async (req, res) => {
     try {
 
@@ -26,8 +27,34 @@ router.get('/', async (req, res) => {
     }
 });
 
+// All Users
+// WORKS but calendar is not displayed
+router.get('/users', async (req, res) => {
+  try {
+
+      const workData = await Work.findAll({
+          include: [
+              {
+                  model: User,
+                  attributes: ['first_name'],
+              },
+          ],
+      });
+      // Serialize data so the template can read it
+      const workers = workData.map((work) => work.get({ plain: true }));
+      // Pass serialized data and session flag into template
+      res.render('calendar', {
+          workers,
+          logged_in: req.session.logged_in
+      });
+  } catch (err) {
+      res.status(500).json(err);
+  }
+});
+
 // Find individual employees schedule
-router.get('/user/:id', async (req, res) => {
+// WORKS
+router.get('/users/:id', async (req, res) => {
     try {
         const workData = await Work.findByPk(req.params.id, {
             include: [
@@ -40,7 +67,7 @@ router.get('/user/:id', async (req, res) => {
 
         const work = workData.get({ plain: true });
 
-        res.render('calender', {
+        res.render('dashboard', {
             ...work,
             logged_in: req.session.logged_in
         });
@@ -91,16 +118,18 @@ router.get('/schedule', withAuth, async (req, res) => {
   });
 
   // Sign-up page
+  // WORKS
 router.get('/signup', (req, res) => {
 
   res.render('signup');
 });
 
 // If the user is already logged in, redirect the request to another route
-router.get('/home', (req, res) => {
+// doesnt work
+router.get('/', (req, res) => {
 
     if (req.session.logged_in) {
-        res.redirect('/dashboard');
+        res.redirect('dashboard');
         return;
     }
 
