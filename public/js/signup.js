f34a5416969ee705add56a9a8cc8d81288dacf4f
Beau4ta <beaufortier13@gmail.com>
2021-06-01 21:15:15 -0400

    signup error

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,14 +1,15 @@
 const signupForm = async (event) => {
     event.preventDefault();
 
-    const username = $('.form-user').val().trim();
+    const first = $('.form-first').val().trim();
+    const last = $('.form-last').val().trim();
     const email = $('.form-email').val().trim();
     const password = $('.form-password').val().trim();
 
-    if (username && email && password) {
-        const response = await fetch('/api/users', {
+    if (first && last && email && password) {
+        const response = await fetch('/api/users/', {
           method: 'POST',
-          body: JSON.stringify({ username, email, password }),
+          body: JSON.stringify({ first, last, email, password }),
           headers: { 'Content-Type': 'application/json' },
         });
     
