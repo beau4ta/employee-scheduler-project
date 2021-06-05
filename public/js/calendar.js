f118d7b57f78c2bd71b0f620609958422019bfa3
Kamille <tramia25@gmail.com>
2021-06-05 10:09:16 -0400

    added get route

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,11 +1,16 @@
-
+const breakblock = document.getElementById('break');
+// const gymblock = document.getElementById('gym');
+// const studyblock = document.getElementById('study');
+// const tvblock = document.getElementById('tv');
+// const friendsblock = document.getElementById('friends');
+const workblock = document.getElementById('work');
 const deselectBtn = document.getElementById('deselect');
-const blockContainer = document.querySelector('.blockContainer');
-const scheduleContainer = document.querySelector('.schedule_container');
+const blockContainer = document.querySelector('.block__container');
+const scheduleContainer = document.querySelector('.schedule__container');
 const resetBtn = document.querySelector('.deleteBtn');
 const popUp = document.querySelector('.pop-up__container');
-const noBtn = document.getElementById('btn_no');
-const yesBtn = document.getElementById('btn_yes');
+const noBtn = document.getElementById('btn__no');
+const yesBtn = document.getElementById('btn__yes');
 
 let selectedColor, active;
 
@@ -16,6 +21,41 @@ deselectBtn.addEventListener('click', resetblocks);
 resetBtn.addEventListener('click',openPopup);
 noBtn.addEventListener('click', closePopup);
 yesBtn.addEventListener('click', deleteblocks);
+
+// blocks click  (3)
+function selectblock (e){
+    resetblocks()
+
+    blockColor = e.target.style.backgroundColor;
+
+    switch(e.target.id){
+        case 'break':
+            activeblock(breakblock, blockColor);
+            icon = '<i class="fas fa-couch"></i>';
+            break
+        case 'gym':
+            activeblock(gymblock, blockColor);
+            icon = '<i class="fas fa-dumbbell"></i>';
+            break
+        case 'study':
+            activeblock(studyblock, blockColor);
+            icon = '<i class="fas fa-book"></i>';
+            break
+        case 'tv':
+            activeblock(tvblock, blockColor);
+            icon = '<i class="fas fa-tv"></i>';
+            break
+        case 'friends':
+            activeblock(friendsblock, blockColor);
+            icon = '<i class="fas fa-users"></i>';
+            break
+        case 'work':
+            activeblock(workblock, blockColor);
+            icon = '<i class="fas fa-briefcase"></i>';
+            break
+    }
+
+};
 
 // Set colors for schedule (4)
 function setColors (e){
